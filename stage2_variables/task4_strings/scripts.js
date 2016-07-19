319cfbeae0658bf2ae650a1b578ec7fecb480746
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-20 01:31:48 +0300

    update stage2 task4

--- stage2_variables/task4_strings/scripts.js
+++ stage2_variables/task4_strings/scripts.js
@@ -1,5 +1,4 @@
-var passphrase = 'Open Sesame';
-console.log(passphrase.length);
-console.log(passphrase.toLowerCase());
-console.log(passphrase);
-console.log(passphrase.toUpperCase());
+var stringToShout = prompt("What should I shout?")
+var shout = stringToShout.toUpperCase();
+shout += "!!!";
+alert(shout);
