9a0bbabdd5172e6575b1bc43c7b6dabf7ba8a88d
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-19 21:53:59 +0300

    stage2 task3 - combining strings

---
+++ stage2_variables/task3_combining_strings/scripts.js
@@ -0,0 +1,6 @@
+var visitor = prompt('What is your name?');
+var message = 'Hello ' + visitor + ', welcome.';
+message = message + ' We are so glad that you came by to visit, ';
+message += visitor;
+message += ". Please come again.";
+document.write(message);
