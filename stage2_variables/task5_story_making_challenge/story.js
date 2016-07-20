5fcebacb4d8879d49bb50756dcc57476ba745bb8
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-20 16:07:10 +0300

    update story making challenge
    add info about how many questions left to each alert box

--- stage2_variables/task5_story_making_challenge/story.js
+++ stage2_variables/task5_story_making_challenge/story.js
@@ -1,10 +1,16 @@
-"<h2>There once was a [adjective] programmer who wanted to use JavaScript to [verb] the [noun].</h2>"
+// There once was a [adjective] programmer who wanted to use JavaScript to [verb] the [noun].
 
-var adjective = prompt("Input an adjective:");
-var sentence = "<h2>There once was a " + adjective;
-var verb = prompt("Input a verb:");
-var noun = prompt("Input a noun:");
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]';
+var adjective = prompt("Input an adjective: " + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var verb = prompt("Input a verb: " + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt("Input a noun:" + questionsLeft);
 alert("You're done! Ready for the message?");
+var sentence = "<h2>There once was a " + adjective;
 sentence += ' programmer who wanted to use JavaScript to ' + verb;
 sentence += ' the ' + noun + '.</h2>';
 
