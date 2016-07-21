8906e4f37a5ea6cec08450f1965f9b6b05964131
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-22 00:58:49 +0300

    Update Quiz Game
    add comments and refactor the code

--- stage4_decisions/task3_quiz_game/quiz.js
+++ stage4_decisions/task3_quiz_game/quiz.js
@@ -1,41 +1,39 @@
+// quiz begins, no answers correct
 var correctAnswers = 0;
 alert('Please answer the 5 following questions.');
 
-var question1 = prompt('How much seconds does it take for light to reach the Moon from the Earth?');
-if (parseInt(question1) === 1) {
+// ask questions
+var answer1 = prompt('How much seconds does it take for light to reach the Moon from the Earth?');
+if (parseInt(answer1) === 1) {
+    correctAnswers += 1;
+}
+var answer2 = prompt('What is the capital of Australia?');
+if (answer2.toUpperCase() === 'CANBERRA') {
+    correctAnswers += 1;
+}
+var answer3 = prompt('A brick weighs 1kg plus a half of a brick. How much kilograms does a brick weigh?');
+if (parseInt(answer3) === 2) {
+    correctAnswers += 1;
+}
+var answer4 = prompt('What is the most popular programming language in 2016?');
+if (answer4.toUpperCase() === 'JAVA') {
+    correctAnswers += 1;
+}
+var answer5 = prompt('What is the major chemical element human body consists of?');
+if (answer5.toUpperCase() === 'OXYGEN' || answer5.toUpperCase() === 'O2' || answer5.toUpperCase() === 'O') {
     correctAnswers += 1;
 }
 
-var question2 = prompt('What is the capital of Australia?');
-if (question2.toUpperCase() === 'CANBERRA') {
-    correctAnswers += 1;
-}
+// output results
+document.write('<p>You got ' + correctAnswers + ' out of 5 questions correct</p>');
 
-var question3 = prompt('A brick weighs 1kg plus a half of a brick. How much kilograms does a brick weigh?');
-if (parseInt(question3) === 2) {
-    correctAnswers += 1;
-}
-
-var question4 = prompt('What is the most popular programming language?');
-if (question4.toUpperCase() === 'JAVA') {
-    correctAnswers += 1;
-}
-
-var question5 = prompt('What is the major chemical element human body consists of?');
-if (question5.toUpperCase() === 'OXYGEN' || question5 === 'O2' || question5 === 'O') {
-    correctAnswers += 1;
-}
-
+// output rank
 if (correctAnswers === 5) {
-    document.write('<p>Congratulations! You have ' + correctAnswers + ' correct answers</p>');
-    document.write('<p>You won a GOLD medal!</p>');
-} else if (3 <= correctAnswers <= 4) {
-    document.write('<p>Congratulations! You have ' + correctAnswers + ' correct answers</p>');
-    document.write('<p>You won a SILVER medal!</p>');
-} else if (1 <= correctAnswers <= 2) {
-    document.write('<p>You have ' + correctAnswers + ' correct answers</p>');
-    document.write('<p>You won a BRONZE medal!</p>');
+    document.write('<p><strong>You won a GOLD medal!</strong></p>');
+} else if (correctAnswers >= 3) {
+    document.write('<p><strong>You won a SILVER medal!</strong></p>');
+} else if (correctAnswers >= 1) {
+    document.write('<p><strong>You won a BRONZE medal!</strong></p>');
 } else {
-    document.write('<p>Sorry, you have ' + correctAnswers + ' right answers.</p>')
-    document.write('<p>Try again later.</p>')
+    document.write('<p><strong>Sorry, you need to study more, try again later.</strong></p>')
 }
