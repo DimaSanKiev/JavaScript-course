f3c416a5559cfe0556606969027c251faa4cae84
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-22 16:21:18 +0300

    task 2 - calculating area

--- 5_functions/5-2_parameters/random.js
+++
@@ -1,8 +0,0 @@
-function getRandomNumber(upper) {
-    return Math.floor(Math.random() * upper) + 1;
-}
-
-console.log(getRandomNumber(6));
-console.log(getRandomNumber(100));
-console.log(getRandomNumber(10000));
-console.log(getRandomNumber(2));
