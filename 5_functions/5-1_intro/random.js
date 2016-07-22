a79d67c04224582d1215f4f42e685e685621e1e2
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-22 16:02:38 +0300

    add argument to getRandomNumber() function

--- 5_functions/5-1_intro/random.js
+++
@@ -1,7 +0,0 @@
-function getRandomNumber() {
-    return Math.floor(Math.random() * 6) + 1;
-}
-
-alert(getRandomNumber());
-console.log(getRandomNumber());
-var dieRoll = getRandomNumber();
---
+++ 5_functions/5-2_parameters/random.js
@@ -0,0 +1,8 @@
+function getRandomNumber(upper) {
+    return Math.floor(Math.random() * upper) + 1;
+}
+
+console.log(getRandomNumber(6));
+console.log(getRandomNumber(100));
+console.log(getRandomNumber(10000));
+console.log(getRandomNumber(2));
