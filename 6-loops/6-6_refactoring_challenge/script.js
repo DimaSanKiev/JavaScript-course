1cc1e1c206aa5efada72af7fbad67f1082356119
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-26 01:20:51 +0300

    refactoring #2
    create and call functions

--- 6-loops/6-6_refactoring_challenge/script.js
+++ 6-loops/6-6_refactoring_challenge/script.js
@@ -1,15 +1,25 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
-for (var i = 0; i < 1000; i++) {
-    red = Math.floor(Math.random() * 256);
-    green = Math.floor(Math.random() * 256);
-    blue = Math.floor(Math.random() * 256);
-    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-    html += '<div style="background-color:' + rgbColor + '"></div>';
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
 }
 
-document.write(html);
+function randomColor() {
+    var color = 'rgb(';
+    color += randomRGB() + ',';
+    color += randomRGB() + ',';
+    color += randomRGB() + ')';
+    return color;
+}
+
+function print(message) {
+    document.write(message);
+}
+
+for (var i = 0; i < 1000; i++) {
+    rgbColor = randomColor();
+    html += '<div style="border: hidden; background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
