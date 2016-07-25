e23f29781d91274f2301a576363c1c7251789eea
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-25 23:30:32 +0300

    do-while loop

--- 6-loops/6-2_loop_conditions/app.js
+++ 6-loops/6-2_loop_conditions/app.js
@@ -1,8 +1,8 @@
 /*
  1. Generate a random number from 1 to 10000. This is the number the computer need to guess.
  2. Enter a while loop - inside this loop the computer will "guess" a random number. If the number
- the computer guesses matches the number generated at the beginning of the program, the loop ends,
- if not, the loop continues until the correct number is guessed.
+    the computer guesses matches the number generated at the beginning of the program, the loop ends,
+    if not, the loop continues until the correct number is guessed.
  3. Exit the loop and print the random number and the number of times the computer to guess it.
  */
 
