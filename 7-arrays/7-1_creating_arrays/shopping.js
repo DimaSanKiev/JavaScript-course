a023dee5e7b0051b9f70a3e7eb7f430ebfd72586
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-26 10:59:15 +0300

    accessing array items

--- 7-arrays/7-1_creating_arrays/shopping.js
+++ 7-arrays/7-1_creating_arrays/shopping.js
@@ -4,4 +4,8 @@ var shoppingList = [
     'eggs'
 ];
 
-document.write(shoppingList);
+// Accessing array items using an index value
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+console.log(shoppingList[3]);   // undefined
