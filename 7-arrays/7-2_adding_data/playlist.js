41dab659441213de6182df9350794eb74599d84d
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-26 13:44:36 +0300

    adding elements to an array
    using push() and unshift() methods

---
+++ 7-arrays/7-2_adding_data/playlist.js
@@ -0,0 +1,11 @@
+var playlist = [];
+
+// The push() method adds one or more elements to the end of an array and returns the new length of the array.
+playlist.push('Anthem');
+playlist.push('Angel\'s Love', 'Black Is The Color');
+
+// The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+playlist.unshift('Filmic');
+playlist.unshift('Time Is The Healer', 'Trigger');
+
+printList(playlist);
