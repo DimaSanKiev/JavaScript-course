7d62c8fb03666d1c7d5067f415b11130d39e81d4
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-27 14:36:42 +0300

    refactor code appearance

--- 8-objects/8-3_objects_array/single-line.js
+++ 8-objects/8-3_objects_array/single-line.js
@@ -4,18 +4,9 @@
  */
 
 var questions = [
-    {
-        question: 'What is the name of a markup language for describing web documents (web pages)?',
-        answer: 'HTML'
-    },
-    {
-        question: 'What language describes how HTML elements are to be displayed on screen, paper, or in other media?',
-        answer: 'CSS'
-    },
-    {
-        question: 'What is the name of a powerful and popular language for programming on the web?',
-        answer: 'JavaScript'
-    },
+    {question: 'What is the name of a language for describing web documents?', answer: 'HTML'},
+    {question: 'What language describes how HTML elements are to be displayed?', answer: 'CSS'},
+    {question: 'What is the name of language for programming on the web?', answer: 'JavaScript'},
     {question: 'What is a name of standard language for accessing databases?', answer: 'SQL'},
     {question: 'What is the most popular programming language?', answer: 'Java'}
 ];
