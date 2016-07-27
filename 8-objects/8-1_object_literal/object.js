cd9800a7a011f1f2c6b6e6e7a7f7c09cd40a5596
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-27 11:48:13 +0300

    accessing object properties

--- 8-objects/8-1_object_literal/object.js
+++ 8-objects/8-1_object_literal/object.js
@@ -1,9 +1,29 @@
-// Example of a JavaScript object
+/*
+ Example of a JavaScript object
+ */
 
+// creating object
 var person = {
     name: 'Jenny',
-    country: 'US',
+    country: 'Brazil',
     age: 25,
     student: false,
     skills: ['JavaScript', 'HTML', 'CSS']
 };
+
+
+function print(message) {
+    var div = document.getElementById('output');
+    div.innerHTML = message;
+}
+
+// accessing object properties
+var message = '<p>Hello. My name is ' + person.name + '</p>';
+message += '<p>I live in ' + person.country + '</p>';
+person.name = 'Rainbow Dash';
+message += '<p>But, I wish my name was ' + person.name + '</p>';
+person.age += 1;
+message += '<p>My age is now ' + person.age + '</p>';
+message += '<p>I have ' + person.skills.length + ' skills: ';
+message += person.skills.join(', ') + '</p>';
+print(message);
