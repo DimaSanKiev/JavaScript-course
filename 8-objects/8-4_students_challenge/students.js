459e613d372d6c95a628924e144248592f4ce23c
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-27 23:46:35 +0300

    students challenge #1
    create a data structure to hold information about students

---
+++ 8-objects/8-4_students_challenge/students.js
@@ -0,0 +1,32 @@
+var students = [
+    {
+        name: 'Chris',
+        track: 'Web Development',
+        achievements: '258',
+        points: '13560'
+    },
+    {
+        name: 'Dennis',
+        track: 'Back End Development',
+        achievements: '279',
+        points: '12115'
+    },
+    {
+        name: 'Andrew',
+        track: 'iOS Development with Swift',
+        achievements: '175',
+        points: '14775'
+    },
+    {
+        name: 'John',
+        track: 'Web Design',
+        achievements: '145',
+        points: '14005'
+    },
+    {
+        name: 'Jordan',
+        track: 'Rails Development',
+        achievements: '294',
+        points: '14540'
+    }
+];
